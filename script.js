4b458324b1a2355b0c0781a311c10752ddf7233f
shreyanagriya <nag.stu2018@juniv.edu>
2023-01-18 14:24:32 -0800

    created a grade-calculator

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-let count = 0;
-
-function setCount() {
-  document.getElementById("output").innerText = count;
+function setInitialValue() {
+  document.getElementById("marks-inp").value = 0;
 }
 
-function setInitialCount() {
-  setCount();
-}
-
-function increment() {
-  count++;
-  setCount();
-  if (count > 10) {
-    alert("Stoppppppppppp");
+function calculateGrade() {
+  const marks = parseInt(document.getElementById("marks-inp").value);
+  if (marks > 100 || marks < 0) {
+    alert("Invalid number..");
+  } else if (marks >= 80 && marks <= 100) {
+    document.getElementById("grade").innerText = "A+";
+  } else if (marks >= 70 && marks <= 79) {
+    document.getElementById("grade").innerText = "A";
+  } else if (marks >= 60 && marks <= 69) {
+    document.getElementById("grade").innerText = "A-";
+  } else if (marks >= 50 && marks <= 59) {
+    document.getElementById("grade").innerText = "B";
+  } else if (marks >= 33 && marks <= 49) {
+    document.getElementById("grade").innerText = "C";
+  } else {
+    document.getElementById("grade").innerText = "F";
   }
 }
 
-setInitialCount();
+setInitialValue();
